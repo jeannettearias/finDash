32c7788a62b54f258b2fb1e57a293b1d80f6d9d5
jeannettearias <jeannettearias@icloud.com>
2025-05-26 21:17:50 +0200

    menu style adjusts

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -18,7 +18,6 @@ function Header() {
                         <img
                             className='calendar__icon'
                             src="/images/calendar.svg" alt="" />
-
                     </div >
                     <div className='search__container'>
                         <input className='inputSearch'
@@ -37,20 +36,22 @@ function Header() {
                             className='logo__title'
                         >Dashboard</h1>
                     </div>
-                    <div>
-                        <h2 className='menu__title'>Menu</h2>
-                        <ul>
-                            <li>Dashboard</li>
-                            <li>Notifications</li>
-                            <li>Wallets</li>
-                        </ul>
-                    </div>
-                    <div>
-                        <h2>Support</h2>
-                        <ul>
-                            <li>Settings</li>
-                            <li>Help</li>
-                        </ul>
+                    <div className='menu-support__container'>
+                        <div className='menu__container'>
+                            <h2 className='menu__title'>Menu</h2>
+                            <ul className='menu__items'>
+                                <li className='selected__item menu__item'>Dashboard</li>
+                                <li className='menu__item'>Notifications</li>
+                                <li className='menu__item'>Wallets</li>
+                            </ul>
+                        </div>
+                        <div className='support__container'>
+                            <h2 className='support__title'>Support</h2>
+                            <ul className='support__items'>
+                                <li className='support__item'>Settings</li>
+                                <li className='support__item'>Help</li>
+                            </ul>
+                        </div>
                     </div>
                 </section>
             </header>
