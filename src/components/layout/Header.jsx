4125bb37f2cff72a978d03eab1a34652813b9010
jeannettearias <jeannettearias@icloud.com>
2025-05-31 22:50:51 +0200

    add style to header

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -3,42 +3,35 @@ import '../../styles/layout/_header.scss';
 function Header() {
     return (
         <>
-            <header className='header'>
-                <section className='top-bar'>
-                    <div className='logo__container'>
-                        <img
-                            className='logo__side__bar'
-                            src="/images/Logo-temp.svg" alt="" />
-                        <h1
-                            className='logo__title'
-                        >Dashboard</h1>
-                    </div>
-                    <div
-                        className='datetime__container'
-                    >
-                        <input
-                            className='datetime__input'
+            <section className='header'>
+                <div className='logo__container'>
+                    <img className='logo__side__bar'
+                        src="/images/Logo-temp.svg" alt="" />
+                    <h1 className='logo__title'>FinDash</h1>
+                </div>
+                <div className='datetime__container'>
+                    <p className='datetime__title'>Dashboard</p>
+                    <div className='input__datepicker'>
+                        <input className='datetime__input'
                             type="text"
                             name="date"
                             id="date"
                             placeholder='May 2025'
                         ></input>
-                        <img
-                            className='calendar__icon'
+                        <img className='calendar__icon'
                             src="/images/calendar.svg" alt="" />
-                    </div >
-                    <div className='search__container'>
-                        <input className='inputSearch'
-                            type="text" placeholder='search' />
-                        <img
-                            className='search__icon'
-                            src="/images/basil_search-outline.svg" alt="" />
                     </div>
-                </section>
-            </header>
+                </div>
+                <div className='search__container'>
+                    <input className='inputSearch'
+                        type="text" placeholder='Search' />
+                    <img
+                        className='search__icon'
+                        src="/images/basil_search-outline.svg" alt="" />
+                </div>
+            </section>
         </>
     )
 }
-
 export default Header;
 
