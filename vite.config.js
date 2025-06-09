a8e560de9a50a8e6f394e9cc039ea20aa03b69ba
jeannettearias <jeannettearias@icloud.com>
2025-06-09 23:48:49 +0200

    Clean gh-pages cache and fix deployment setup

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "./",
+  base: "/finDash/", // <-- This is the key fix
   server: {
     open: "/",
     watch: {
@@ -12,3 +12,5 @@ export default defineConfig({
     }
   }
 });
+
+
